7e1298276aebb44536a24b8bd3c96800ddd94455
Fernando Barillas <fbis251@mailbox.org>
2016-04-27 21:39:10 -0700

    RedditService: Cleanup

--- redditservice/src/main/java/com/fernandobarillas/redditservice/RedditService.java
+++ redditservice/src/main/java/com/fernandobarillas/redditservice/RedditService.java
@@ -30,21 +30,22 @@ import rx.android.schedulers.AndroidSchedulers;
 import rx.schedulers.Schedulers;
 
 public class RedditService extends Service {
-    public static final String USER_AGENT_KEY = "user_agent";
-    public static final String REFRESH_TOKEN_KEY = "refresh_token";
-    public static final String REDDIT_CLIENT_ID_KEY = "reddit_client_id";
-    public static final String REDDIT_REDIRECT_URL_KEY = "reddit_redirect_url";
-    public static final int RELOAD_LIMIT = 50; // How many links cached before a reload is triggered
-    private static final String LOG_TAG = "RedditService";
+    public static final  String REDDIT_BASE_URL         = "https://reddit.com";
+    public static final  String USER_AGENT_KEY          = "user_agent";
+    public static final  String REFRESH_TOKEN_KEY       = "refresh_token";
+    public static final  String REDDIT_CLIENT_ID_KEY    = "reddit_client_id";
+    public static final  String REDDIT_REDIRECT_URL_KEY = "reddit_redirect_url";
+    public static final  int    RELOAD_LIMIT            = 50; // How many links cached before a reload is triggered
+    private static final String LOG_TAG                 = "RedditService";
 
     private final IBinder mIBinder = new RedditBinder();
 
     private ServicePreferences mServicePreferences;
-    private RedditData mRedditData;
+    private RedditData         mRedditData;
 
     public RedditService() {
         Log.d(LOG_TAG, "RedditService() instantiated");
-        if(mServicePreferences == null) return;
+        if (mServicePreferences == null) return;
         // TODO: Allow the redditService to handle the instance of a subredditRequest to allow easy access to a single subreddit paginator
 
         Log.d("RedditService", "Getting RedditData instance");
@@ -52,6 +53,29 @@ public class RedditService extends Service {
                                              mServicePreferences.getRefreshToken(),
                                              mServicePreferences.getRedditClientId(),
                                              mServicePreferences.getRedditRedirectUrl());
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        Log.d(LOG_TAG, "onBind() called with: " + "intent = [" + intent + "]");
+        initializeService(intent);
+
+        return mIBinder;
+    }
+
+    @Override
+    public void onDestroy() {
+        Log.d(LOG_TAG, "onDestroy()");
+        super.onDestroy();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.d(LOG_TAG,
+              "onStartCommand() called with: " + "intent = [" + intent + "], flags = [" + flags + "], startId = [" + startId + "]");
+        initializeService(intent);
+
+        return Service.START_NOT_STICKY;
     }
 
     public void downvoteLink(Link link, RedditVoteCallback voteCallback) {
@@ -91,54 +115,6 @@ public class RedditService extends Service {
         mRedditData.getNewLinks(subredditRequest);
     }
 
-    private void initializeService(Intent intent) {
-        Log.v(LOG_TAG, "initializeService() called with: " + "intent = [" + intent + "]");
-
-        Context serviceContext = this;
-        mServicePreferences = new ServicePreferences(serviceContext);
-
-        String userAgentString = intent.getExtras().getString(USER_AGENT_KEY, "");
-        String refreshToken = intent.getExtras().getString(REFRESH_TOKEN_KEY);
-        String redditClientId = intent.getExtras().getString(REDDIT_CLIENT_ID_KEY);
-        String redditRedirectUrl = intent.getExtras().getString(REDDIT_REDIRECT_URL_KEY);
-
-        mServicePreferences.setUserAgentString(userAgentString);
-        mServicePreferences.setRefreshToken(refreshToken);
-        mServicePreferences.setRedditClientId(redditClientId);
-        mServicePreferences.setRedditRedirectUrl(redditRedirectUrl);
-
-        // TODO: Handle null extras
-        mRedditData = RedditData.getInstance(UserAgent.of(mServicePreferences.getUserAgentString()),
-                                             mServicePreferences.getRefreshToken(),
-                                             mServicePreferences.getRedditClientId(),
-                                             mServicePreferences.getRedditRedirectUrl());
-
-        // TODO: Force new instance of reddit data for new user logins/user switching
-    }
-
-    @Override
-    public int onStartCommand(Intent intent, int flags, int startId) {
-        Log.d(LOG_TAG,
-              "onStartCommand() called with: " + "intent = [" + intent + "], flags = [" + flags + "], startId = [" + startId + "]");
-        initializeService(intent);
-
-        return Service.START_NOT_STICKY;
-    }
-
-    @Override
-    public void onDestroy() {
-        Log.d(LOG_TAG, "onDestroy()");
-        super.onDestroy();
-    }
-
-    @Override
-    public IBinder onBind(Intent intent) {
-        Log.d(LOG_TAG, "onBind() called with: " + "intent = [" + intent + "]");
-        initializeService(intent);
-
-        return mIBinder;
-    }
-
     public void removeVote(Link link, RedditVoteCallback voteCallback) {
         Log.d(LOG_TAG,
               "removeVote() called with: " + "link = [" + link + "], voteCallback = [" + voteCallback + "]");
@@ -170,8 +146,7 @@ public class RedditService extends Service {
     }
 
     public Subscription getUserSubreddits(final RedditSubscriptionsCallback subscriptionsCallback) {
-        Observable<List<Subreddit>> subredditsObservable =
-                mRedditData.getUserSubredditsObservable();
+        Observable<List<Subreddit>> subredditsObservable = mRedditData.getUserSubredditsObservable();
         final HashSet<Subreddit> subredditsSet = new HashSet<>();
         return subredditsObservable.subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
@@ -202,6 +177,31 @@ public class RedditService extends Service {
                         subredditsSet.addAll(subreddits);
                     }
                 });
+    }
+
+    private void initializeService(Intent intent) {
+        Log.v(LOG_TAG, "initializeService() called with: " + "intent = [" + intent + "]");
+
+        Context serviceContext = this;
+        mServicePreferences = new ServicePreferences(serviceContext);
+
+        String userAgentString   = intent.getExtras().getString(USER_AGENT_KEY, "");
+        String refreshToken      = intent.getExtras().getString(REFRESH_TOKEN_KEY);
+        String redditClientId    = intent.getExtras().getString(REDDIT_CLIENT_ID_KEY);
+        String redditRedirectUrl = intent.getExtras().getString(REDDIT_REDIRECT_URL_KEY);
+
+        mServicePreferences.setUserAgentString(userAgentString);
+        mServicePreferences.setRefreshToken(refreshToken);
+        mServicePreferences.setRedditClientId(redditClientId);
+        mServicePreferences.setRedditRedirectUrl(redditRedirectUrl);
+
+        // TODO: Handle null extras
+        mRedditData = RedditData.getInstance(UserAgent.of(mServicePreferences.getUserAgentString()),
+                                             mServicePreferences.getRefreshToken(),
+                                             mServicePreferences.getRedditClientId(),
+                                             mServicePreferences.getRedditRedirectUrl());
+
+        // TODO: Force new instance of reddit data for new user logins/user switching
     }
 
     public class RedditBinder extends Binder {
