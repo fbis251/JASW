7ae9c59655158cdb41746cfa932e1a638593dbb6
Fernando Barillas <fbis251@mailbox.org>
2016-05-15 01:28:52 -0700

    UserSubscriptions: Refactor and cleanup

--- redditservice/src/main/java/com/fernandobarillas/redditservice/observables/UserSubscriptions.java
+++ redditservice/src/main/java/com/fernandobarillas/redditservice/observables/UserSubscriptions.java
@@ -2,12 +2,12 @@ package com.fernandobarillas.redditservice.observables;
 
 import android.util.Log;
 
+import com.fernandobarillas.redditservice.data.RedditLinks;
+
 import net.dean.jraw.RedditClient;
 import net.dean.jraw.models.Listing;
 import net.dean.jraw.models.Subreddit;
 import net.dean.jraw.paginators.UserSubredditsPaginator;
-
-import java.util.List;
 
 import rx.Observable;
 import rx.Subscriber;
@@ -15,36 +15,35 @@ import rx.Subscriber;
 /**
  * An RxJava Observable that downloads a reddit User's subreddit subscriptions.
  */
-public class UserSubredditsObservable {
+public class UserSubscriptions {
     private static final String LOG_TAG = "UserSubredditsObs";
 
     private RedditClient mRedditClient;
 
-    public UserSubredditsObservable(RedditClient redditClient) {
+    public UserSubscriptions(RedditClient redditClient) {
         Log.v(LOG_TAG,
-              "UserSubredditsObservable() called with: " + "redditClient = [" + redditClient + "]");
+              "UserSubscriptions() called with: " + "redditClient = [" + redditClient + "]");
         mRedditClient = redditClient;
     }
 
-    public Observable<List<Subreddit>> getObservable() {
-        return Observable.create(new Observable.OnSubscribe<List<Subreddit>>() {
+    public Observable<Subreddit> getSubscriptions() {
+        return Observable.create(new Observable.OnSubscribe<Subreddit>() {
             @Override
-            public void call(Subscriber<? super List<Subreddit>> subscriber) {
-                Log.v(LOG_TAG, "getObservable call() called with: " + "subscriber = [" +
-                        subscriber + "]");
-                UserSubredditsPaginator paginator =
-                        new UserSubredditsPaginator(mRedditClient, "subscriber");
-                // TODO: Will this request work?
-                paginator.setLimit(100);
+            public void call(Subscriber<? super Subreddit> subscriber) {
+                Log.v(LOG_TAG, "downloadSubscriptions()");
+                UserSubredditsPaginator paginator = new UserSubredditsPaginator(mRedditClient,
+                                                                                "subscriber");
+                paginator.setLimit(RedditLinks.MAX_LINK_LIMIT);
                 try {
                     while (paginator.hasNext()) {
                         // Ensure we still have a subscriber to emit data to
-                        if (subscriber.isUnsubscribed()) return;
 
                         Listing<Subreddit> subreddits = paginator.next(true);
                         if (subreddits != null && subreddits.getChildren() != null) {
                             // Pass on the subreddits as the network calls return them
-                            subscriber.onNext(subreddits.getChildren());
+                            for (Subreddit subreddit : subreddits) {
+                                subscriber.onNext(subreddit);
+                            }
                         }
                     }
 
