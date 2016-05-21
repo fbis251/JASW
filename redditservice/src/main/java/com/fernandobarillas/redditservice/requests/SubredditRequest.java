385ce1822785e4b86cf76827c0756c40383c96da
Fernando Barillas <fbis251@mailbox.org>
2016-05-21 02:29:19 -0700

    SubredditRequest: Add checks for TimePeriod
    * Some requests don't support certain TimePeriod/Sorting combinations

--- redditservice/src/main/java/com/fernandobarillas/redditservice/requests/SubredditRequest.java
+++ redditservice/src/main/java/com/fernandobarillas/redditservice/requests/SubredditRequest.java
@@ -1,7 +1,5 @@
 package com.fernandobarillas.redditservice.requests;
 
-import android.support.annotation.NonNull;
-
 import com.fernandobarillas.redditservice.callbacks.RedditLinksCallback;
 import com.fernandobarillas.redditservice.links.validators.LinkValidator;
 
@@ -9,25 +7,31 @@ import net.dean.jraw.paginators.Sorting;
 import net.dean.jraw.paginators.TimePeriod;
 
 /**
- * Created by fb on 12/14/15.
+ * Class that facilitates getting a request using JRAW's SubredditPaginator. This class uses a
+ * builder pattern in order to avoid Exceptions with the request being changed after the Paginator
+ * has been instantiated. Created by fb on 12/14/15.
  */
 public class SubredditRequest {
-    private final String mSubreddit;
-    private final Sorting mSorting;
-    private final TimePeriod mTimePeriod;
-    private final int mLinkLimit;
-    private final LinkValidator mLinkValidator;
-    // Only the onComplete should be dynamically changed since it's the only field nto passed in to
-    // the JRAW Paginator which will avoid getting an exception when the Paginator state changes
-    private RedditLinksCallback mRedditLinksCallback;
+    private final String              mSubreddit;
+    private final Sorting             mSorting;
+    private final TimePeriod          mTimePeriod;
+    private final int                 mLinkLimit;
+    private final LinkValidator       mLinkValidator;
+    private       RedditLinksCallback mRedditLinksCallback;
 
     private SubredditRequest(Builder builder) {
         mSubreddit = builder.subreddit;
         mSorting = builder.mSorting;
-        mTimePeriod = builder.mTimePeriod;
         mLinkLimit = builder.mLinkLimit;
         mLinkValidator = builder.mLinkValidator;
         mRedditLinksCallback = builder.mRedditLinksCallback;
+
+        // Certain Sorting types don't support TimePeriods in the reddit API
+        if (mSorting == Sorting.CONTROVERSIAL || mSorting == Sorting.TOP) {
+            mTimePeriod = builder.mTimePeriod;
+        } else {
+            mTimePeriod = null;
+        }
     }
 
     public int getLinkLimit() {
@@ -63,13 +67,13 @@ public class SubredditRequest {
         private final String subreddit;
 
         // Optional parameters, using default values
-        private Sorting mSorting = Sorting.HOT;
-        private TimePeriod mTimePeriod = TimePeriod.DAY;
-        private int mLinkLimit = 100;
-        private LinkValidator mLinkValidator = null;
+        private Sorting             mSorting             = Sorting.HOT;
+        private TimePeriod          mTimePeriod          = TimePeriod.DAY;
+        private int                 mLinkLimit           = 100;
+        private LinkValidator       mLinkValidator       = null;
         private RedditLinksCallback mRedditLinksCallback = null;
 
-        public Builder(@NonNull String subreddit) {
+        public Builder(String subreddit) {
             this.subreddit = subreddit;
         }
 
