10c75b862b87f80c8357aab172f4d28e8f2f6f6c
Fernando Barillas <fbis251@mailbox.org>
2016-07-22 19:33:17 -0700

    UserSubscriptions: Formatting cleanup

--- redditservice/src/main/java/com/fernandobarillas/redditservice/observables/UserSubscriptions.java
+++ redditservice/src/main/java/com/fernandobarillas/redditservice/observables/UserSubscriptions.java
@@ -1,57 +1,55 @@
 package com.fernandobarillas.redditservice.observables;
 
-import android.util.Log;
-
-import com.fernandobarillas.redditservice.data.RedditLinks;
+import com.fernandobarillas.redditservice.RedditService;
 
 import net.dean.jraw.RedditClient;
 import net.dean.jraw.models.Listing;
 import net.dean.jraw.models.Subreddit;
 import net.dean.jraw.paginators.UserSubredditsPaginator;
 
+import java.util.List;
+
 import rx.Observable;
 import rx.Subscriber;
+import timber.log.Timber;
 
 /**
  * An RxJava Observable that downloads a reddit User's subreddit subscriptions.
  */
 public class UserSubscriptions {
-    private static final String LOG_TAG = "UserSubredditsObs";
 
     private RedditClient mRedditClient;
 
     public UserSubscriptions(RedditClient redditClient) {
-        Log.v(LOG_TAG, "UserSubscriptions() called with: " + "redditClient = [" + redditClient + "]");
+        Timber.v("UserSubscriptions() called with: " + "redditClient = [" + redditClient + "]");
         mRedditClient = redditClient;
     }
 
-    public Observable<Subreddit> getSubscriptions() {
-        return Observable.create(new Observable.OnSubscribe<Subreddit>() {
+    public Observable<List<Subreddit>> getSubscriptions() {
+        Timber.v("getSubscriptions() called");
+        return Observable.create(new Observable.OnSubscribe<List<Subreddit>>() {
             @Override
-            public void call(Subscriber<? super Subreddit> subscriber) {
-                Log.v(LOG_TAG, "downloadSubscriptions()");
-                UserSubredditsPaginator paginator = new UserSubredditsPaginator(mRedditClient, "subscriber");
-                paginator.setLimit(RedditLinks.MAX_LINK_LIMIT);
+            public void call(Subscriber<? super List<Subreddit>> subscriber) {
+                Timber.v("getSubscriptions call() called with: "
+                        + "subscriber = ["
+                        + subscriber
+                        + "]");
+                UserSubredditsPaginator paginator =
+                        new UserSubredditsPaginator(mRedditClient, "subscriber");
+                paginator.setLimit(RedditService.MAX_LINK_LIMIT);
                 try {
                     while (paginator.hasNext()) {
                         // Ensure we still have a subscriber to emit data to
-
                         Listing<Subreddit> subreddits = paginator.next(true);
                         if (subreddits != null && subreddits.getChildren() != null) {
                             // Pass on the subreddits as the network calls return them
-                            for (Subreddit subreddit : subreddits) {
-                                subscriber.onNext(subreddit);
-                            }
+                            subscriber.onNext(subreddits.getChildren());
                         }
                     }
 
-                    if (!subscriber.isUnsubscribed()) {
-                        subscriber.onCompleted();
-                    }
+                    if (!subscriber.isUnsubscribed()) subscriber.onCompleted();
                 } catch (Exception e) {
-                    if (!subscriber.isUnsubscribed()) {
-                        subscriber.onError(e);
-                    }
+                    if (!subscriber.isUnsubscribed()) subscriber.onError(e);
                 }
             }
         });
