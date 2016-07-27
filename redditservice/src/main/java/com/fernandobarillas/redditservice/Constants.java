0b8df0f7869c52b894dc31eaca0df2a27522e4f7
Fernando Barillas <fbis251@mailbox.org>
2016-07-26 20:28:12 -0700

    Constants: Add reddit mobile base URL constant

--- redditservice/src/main/java/com/fernandobarillas/redditservice/Constants.java
+++ redditservice/src/main/java/com/fernandobarillas/redditservice/Constants.java
@@ -5,5 +5,7 @@ package com.fernandobarillas.redditservice;
  */
 public class Constants {
     /** The default user agent to use for reddit API requests if no other is specified */
-    public static final String DEFAULT_USER_AGENT = "Android:JASW (by /u/fbis251)";
+    public static final String DEFAULT_USER_AGENT     = "Android:JASW (by /u/fbis251)";
+    /** The base mobile URL for reddit. This contains no trailing slash */
+    public static final String REDDIT_MOBILE_BASE_URL = "https://m.reddit.com";
 }
